8406892234089bd0bb317131699388479c3da8c8
smyk5537 <80877926+smyk5537@users.noreply.github.com>
2021-06-28 01:30:45 +0900

    구분선,노트,판정선 생성 이동

---
+++ Unity 2D RythmGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // GameManager ΩÃ±€≈Ê √≥∏Æ
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    public float noteSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
